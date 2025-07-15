1956afcbcdb1aa47ad5671db8fdfdb555fbd1700
SayanIndra83 <sayanindra0@gmail.com>
2025-07-15 22:23:37 +0530

    new update

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,3 @@
-
 const addEventOnElements = function (elements, eventType, callback) {
   for (let i = 0, len = elements.length; i < len; i++) {
     elements[i].addEventListener(eventType, callback);
